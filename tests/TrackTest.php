3595d7eff09eb97917253c9bbc9c738d061adf27
Nick <git@phpfanatic.com>
2017-02-21 02:28:36 -0500

    Removed 3rd test until cURL plays with Travis.

--- tests/TrackTest.php
+++ tests/TrackTest.php
@@ -31,17 +31,4 @@ class TrackTest extends PHPUnit_Framework_TestCase {
 		$this->expectException(\Exception::class);
 		$this->api->ApiCall('UpdateLeadPrice', array());
 	}
-	
-	/**
-	 * Test that curl is sending and responding.  
-	 * Placeholder url returns json string.
-	 */
-	public function testSendRequest() {
-		$this->api->SetApiKey('test123');
-		$this->api->SetApiUrl('https://jsonplaceholder.typicode.com/posts');
-		$this->api->BuildUri('testFunction');
-		$response = $this->api->SendRequest();
-		
-		$this->assertInternalType('string', $response, "Got a " . gettype($response) . " instead of a string.");
-	}
 }
